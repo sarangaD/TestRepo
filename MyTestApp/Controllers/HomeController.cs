d4bab15e4803dc035e5ffd8db91e972f33cef7ea
sarangaD <saranga.ucsc@gmail.com>
2018-07-19 15:34:47 +0530

    first commit

---
+++ MyTestApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MyTestApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Ajaxmethod(PersonModel person)
+        {
+            return Json(person);
+        }
+    }
+}
